83c2ee4ed4bac2fa8e5bd31b94a58262b7fc1f6f
Vishnu Jangid <vishnu.jangid@intimetec.com>
2020-05-18 17:51:22 +0530

    Made Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component'
+import { EmployeeComponent } from './employee/employee.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'employee', component: EmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
